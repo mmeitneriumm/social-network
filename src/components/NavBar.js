bc505a22fa3ae560a8c37ce6380613c517fab120
mmeitneriumm <mmeitneriumm@gmail.com>
2021-11-21 14:40:01 +0300

    add icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,33 @@
 import "./NavBar.css";
+import { FaUserAlt } from "react-icons/fa";
+import { FaComments } from "react-icons/fa";
+import { FaPhotoVideo } from "react-icons/fa";
+import { FaMusic } from "react-icons/fa";
+import { FaSlidersH } from "react-icons/fa";
 
 const NavBar = () => {
   return (
     <div className="nav">
-      <div>My profile</div>
-      <div>Picture exchange</div>
-      <div>World gallery</div>
-      <div>Music (why not?)</div>
-      <div>Settings</div>
+      <div className="my-profile">
+        <FaUserAlt />
+        <a href="#">My profile</a>
+      </div>
+      <div className="my-connect">
+        <FaComments />
+        <a href="#">Connect</a>
+      </div>
+      <div className="my-world-gallery">
+        <FaPhotoVideo />
+        <a href="#">World gallery</a>
+      </div>
+      <div className="my-music">
+        <FaMusic />
+        <a href="#">Music (why not?)</a>
+      </div>
+      <div className="my-settings">
+        <FaSlidersH />
+        <a href="#">Settings</a>
+      </div>
     </div>
   );
 };
