a116fc64f08f3e4a9996d320c949a6a3b9cfcbe1
mmeitneriumm <mmeitneriumm@gmail.com>
2021-11-22 23:53:12 +0300

    edit profilepage

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,8 +3,8 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
   return (
-    <div className={styles.myPicture}>
-      <h2>My posts</h2>
+    <div className={styles.myPosts}>
+      <p>My posts</p>
       <div>New post</div>
       <textarea cols="30" rows="2"></textarea>
       <button>Add post</button>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,19 +5,17 @@ import background from "../img/bg.jpg";
 const Profile = () => {
   return (
     <div>
-      <div>
+      {/* <div>
         <img src={background} alt="background" />
-      </div>
+      </div> */}
       <div className={styles.profileInfo}>
-        <img
-          src="https://sun9-10.userapi.com/impg/Y98ahL09qw8fvGaCUPCJ1taIkWcaGWewN7jIgw/Tsso-6hRbXs.jpg?size=564x564&quality=96&sign=16ae910f5e035029be70272137a4c5d6&type=album"
-          alt=""
-        />
-
         <div className={styles.description}>
-          <h4>Maria Merkushova</h4>
-          <h4>21 y.o</h4>
-          <h4>Frontend-developer</h4>
+          <img
+            src="https://sun9-10.userapi.com/impg/Y98ahL09qw8fvGaCUPCJ1taIkWcaGWewN7jIgw/Tsso-6hRbXs.jpg?size=564x564&quality=96&sign=16ae910f5e035029be70272137a4c5d6&type=album"
+            alt=""
+          />
+          <p className={styles.name}>Mary Storm</p>
+          <p className={styles.skill}>Frontend-developer</p>
         </div>
       </div>
       <MyPosts />
