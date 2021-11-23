b6df2183edad490bb8a1caeee1a7eee346ade938
mmeitneriumm <mmeitneriumm@gmail.com>
2021-11-23 21:38:18 +0300

    верстка диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,21 @@
+import styles from "./Dialogs.module.css";
+
 const Dialogs = () => {
   return (
-    <div>Dialogs ваылволллллллллллллллллллллллллллллллллллллллллллллллд</div>
+    <div className={styles.messanger}>
+      <div className={styles.messages}>
+        <div className={styles.messageItemMe}>Есть чай через руки?</div>
+        <div className={styles.messageItemYou}>Пить морковь без носков?</div>
+        <div className={styles.messageItemMe}>Что</div>
+        <div className={styles.messageItemYou}>Что?</div>
+      </div>
+      <div className={styles.dialogs}>
+        <p>Список диалогов:</p>
+        <div className={styles.dialogsItem}>Хаски Мен</div>
+        <div className={styles.dialogsItemActive}>Никита Кальян</div>
+        <div className={styles.dialogsItem}>Влад Машина</div>
+      </div>
+    </div>
   );
 };
 
