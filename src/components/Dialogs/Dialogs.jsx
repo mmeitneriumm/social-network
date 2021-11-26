f125a0c2c7fa9a828531842958a64dc136823602
mmeitneriumm <mmeitneriumm@gmail.com>
2021-11-26 12:40:59 +0300

    textarea с событием на клик, делаем пополнение даты. часть 1

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./Dialogs.module.css";
 import DialogItem from "./DialogsItem/DialogItem";
 import MessageItem from "./MessageItem/MessageItem";
@@ -11,9 +12,22 @@ const Dialogs = (props) => {
     <MessageItem message={message.message} />
   ));
 
+  let newMessageElement = React.createRef();
+  let addMessage = () => {
+    let text = newMessageElement.current.value;
+    alert(text);
+  };
+
   return (
     <div className={styles.messanger}>
-      <div className={styles.messages}>{messagesElement}</div>
+      <div className={styles.messages}>
+        {messagesElement}
+        <div className={styles.addPost}>
+          <textarea ref={newMessageElement}></textarea>
+          <button onClick={addMessage}>Send message</button>
+        </div>
+      </div>
+
       <div className={styles.dialogs}>
         <p>Список диалогов:</p>
         {dialogsElement}
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,14 +1,20 @@
+import React from "react";
 import styles from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
   let postElement = props.posts.map((p) => <Post message={p.post} />);
+  let newPostElement = React.createRef();
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
   return (
     <div className={styles.myPosts}>
       <p>My posts</p>
       <div>New post</div>
-      <textarea cols="30" rows="2"></textarea>
-      <button>Add post</button>
+      <textarea cols="30" rows="2" ref={newPostElement}></textarea>
+      <button onClick={addPost}>Add post</button>
       {postElement}
     </div>
   );
