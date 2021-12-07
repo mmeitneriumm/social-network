08e24e00ece20d88342ed260d0c114a30530200b
mmeitneriumm <mmeitneriumm@gmail.com>
2021-12-07 15:13:41 +0300

    классовая компонента

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,60 +1,67 @@
+import React from "react";
 import styles from "./Users.module.css";
 import { FaMapMarker } from "react-icons/fa";
 import axios from "axios";
 import user from "./../img/user.jpg";
 
-let Users = (props) => {
-  if (props.users.length === 0) {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        props.setUsers(response.data.items);
-      });
-  }
-  return (
-    <div className={styles.users}>
-      {props.users.map((u) => (
-        <div key={u.id} className={styles.usersItem}>
-          <div className={styles.avaAndName}>
-            <div className={styles.photo}>
-              <img
-                src={u.photos.small != null ? u.photos.small : user}
-                alt="ava"
-              />
+class Users extends React.Component {
+  getUsers = () => {
+    if (this.props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        });
+    }
+  };
+
+  render() {
+    return (
+      <div className={styles.users}>
+        <button onClick={this.getUsers}>get users</button>
+        {this.props.users.map((u) => (
+          <div key={u.id} className={styles.usersItem}>
+            <div className={styles.avaAndName}>
+              <div className={styles.photo}>
+                <img
+                  src={u.photos.small != null ? u.photos.small : user}
+                  alt="ava"
+                />
+              </div>
+              <div className={styles.fullName}>{u.name}</div>
             </div>
-            <div className={styles.fullName}>{u.name}</div>
+            <div className={styles.status}>"{u.status}"</div>
+            <div className={styles.location}>
+              <FaMapMarker />
+              <div>{"u.location.city"}, </div>
+              <div>{"u.location.country"}</div>
+            </div>
+            <div className={styles.buttonBlock}>
+              {u.followed ? (
+                <button
+                  className={styles.button}
+                  onClick={() => {
+                    this.props.unfollow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  className={styles.button}
+                  onClick={() => {
+                    this.props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
           </div>
-          <div className={styles.status}>"{u.status}"</div>
-          <div className={styles.location}>
-            <FaMapMarker />
-            <div>{"u.location.city"}, </div>
-            <div>{"u.location.country"}</div>
-          </div>
-          <div className={styles.buttonBlock}>
-            {u.followed ? (
-              <button
-                className={styles.button}
-                onClick={() => {
-                  props.unfollow(u.id);
-                }}
-              >
-                Unfollow
-              </button>
-            ) : (
-              <button
-                className={styles.button}
-                onClick={() => {
-                  props.follow(u.id);
-                }}
-              >
-                Follow
-              </button>
-            )}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
