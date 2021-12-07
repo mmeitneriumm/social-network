04e07bf407e74f4b5b8dc4563ac8aae9a7dbcb0c
mmeitneriumm <mmeitneriumm@gmail.com>
2021-12-07 13:53:36 +0300

    users api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,34 @@
 import styles from "./Users.module.css";
 import { FaMapMarker } from "react-icons/fa";
+import axios from "axios";
+import user from "./../img/user.jpg";
 
 let Users = (props) => {
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
+  }
   return (
     <div className={styles.users}>
       {props.users.map((u) => (
         <div key={u.id} className={styles.usersItem}>
           <div className={styles.avaAndName}>
             <div className={styles.photo}>
-              <img src={u.photoUrl} alt="ava" />
+              <img
+                src={u.photos.small != null ? u.photos.small : user}
+                alt="ava"
+              />
             </div>
-            <div className={styles.fullName}>{u.fullName}</div>
+            <div className={styles.fullName}>{u.name}</div>
           </div>
           <div className={styles.status}>"{u.status}"</div>
           <div className={styles.location}>
             <FaMapMarker />
-            <div>{u.location.city}, </div>
-            <div>{u.location.country}</div>
+            <div>{"u.location.city"}, </div>
+            <div>{"u.location.country"}</div>
           </div>
           <div className={styles.buttonBlock}>
             {u.followed ? (
