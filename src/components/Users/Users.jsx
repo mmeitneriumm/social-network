0a68af37a438045fd764612bbc433474cd7adf67
mmeitneriumm <mmeitneriumm@gmail.com>
2021-12-07 15:25:32 +0300

    get запрос как метод класса

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,20 +5,19 @@ import axios from "axios";
 import user from "./../img/user.jpg";
 
 class Users extends React.Component {
-  getUsers = () => {
-    if (this.props.users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          this.props.setUsers(response.data.items);
-        });
-    }
-  };
+  constructor(props) {
+    super(props);
+
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
 
   render() {
     return (
       <div className={styles.users}>
-        <button onClick={this.getUsers}>get users</button>
         {this.props.users.map((u) => (
           <div key={u.id} className={styles.usersItem}>
             <div className={styles.avaAndName}>
